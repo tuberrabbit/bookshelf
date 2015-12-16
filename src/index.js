2542617893455aa49754f2139f1a86c7bbd31d1a
tuber <btu@thoughtworks.com>
2015-12-16 16:26:06 +0800

    [tuber] update step1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 var Header = document.querySelector('#Header');
 document.body.appendChild(Header.import.querySelector('.header'));
+var homeButton = document.querySelector('#home');
+homeButton.setAttribute('class', homeButton.getAttribute('class') + ' button--selected-tab');
 
 window.onload = function () {
   var createRowsWithoutRecord = function () {
     var node = document.createElement('div');
-    var value = document.createTextNode('sorry, there is no record yet.')
-    node.appendChild(value)
-    node.setAttribute('class', 'books-list--no-record');
+    node.textContent = 'sorry, there is no record yet.';
+    node.setAttribute('class', 'table--no-record');
     return node;
   };
-  var booksList = document.querySelector('#books-list');
+  var booksList = document.querySelector('#table');
   booksList.appendChild(createRowsWithoutRecord());
 }
