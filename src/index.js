abe2e0de23a1fd479a5e213ad2432a1a62901e3b
tuber <btu@thoughtworks.com>
2015-12-16 13:19:04 +0800

    [tuber] update step 1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-var Header = document.querySelector('#Header')
-document.body.appendChild(Header.import.querySelector('.header'))
+var Header = document.querySelector('#Header');
+document.body.appendChild(Header.import.querySelector('.header'));
+
+window.onload = function () {
+  var createRowsWithoutRecord = function () {
+    var node = document.createElement('div');
+    var value = document.createTextNode('sorry, there is no record yet.')
+    node.appendChild(value)
+    node.setAttribute('class', 'books-list--no-record');
+    return node;
+  };
+  var booksList = document.querySelector('#books-list');
+  booksList.appendChild(createRowsWithoutRecord());
+}
