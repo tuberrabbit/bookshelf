b2ffa0c553b4f68f35e2b8a6d3c3e7a7c201b31e
tuber <btu@thoughtworks.com>
2015-12-17 18:46:46 +0800

    [tuber] complete step2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,50 @@ var homeButton = document.querySelector('#home');
 homeButton.setAttribute('class', homeButton.getAttribute('class') + ' button--selected-tab');
 
 window.onload = function () {
-  var createRowsWithoutRecord = function () {
-    var node = document.createElement('div');
-    node.textContent = 'sorry, there is no record yet.';
-    node.setAttribute('class', 'table--no-record');
-    return node;
+  var tableHeaderList = ['Name', 'Publisher', 'By', 'ISBN', 'Price($)'];
+
+  var createRowWith = function (book) {
+    var tr = document.createElement('tr');
+    if (!book) {
+      tr.textContent = 'sorry, there is no record yet.';
+      tr.setAttribute('class', 'table__row--no-book');
+    } else {
+      tableHeaderList.forEach(function (header) {
+        var td = document.createElement('td');
+        td.textContent = book[header];
+        td.setAttribute('class', 'table__row--has-book');
+        tr.appendChild(td);
+      })
+    }
+    return tr;
   };
-  var booksList = document.querySelector('#table');
-  booksList.appendChild(createRowsWithoutRecord());
+
+  var createTableHeader = function () {
+    var tr = document.createElement('tr');
+    tableHeaderList.forEach(function (header) {
+      var th = document.createElement('th');
+      th.textContent = header;
+      th.setAttribute('class', 'table__row--header');
+      tr.appendChild(th);
+    })
+    return tr;
+  };
+
+  var createTableWith = function (books) {
+    var table = document.createElement('table');
+    table.setAttribute('class', 'table');
+    if (!books) {
+      table.appendChild(createRowWith());
+    } else {
+      table.appendChild(createTableHeader());
+      books.forEach(function (book) {
+        table.appendChild(createRowWith(book));
+      })
+    }
+    return table;
+  };
+
+  var booksList = document.querySelector('#book-list');
+  var books = JSON.parse(localStorage.getItem('books'));
+  booksList.appendChild(createTableWith(books));
 }
